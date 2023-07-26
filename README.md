17d65ce6a87e9a8ceb1684d952df7ba058d4ff68
ovsds <ovsds@yandex-team.ru>
2023-07-26 19:58:08 +0300

    feat: add alternative auth

--- README.md
+++ README.md
@@ -23,6 +23,10 @@ The action issues new Yandex Cloud IAM token and puts it in the output.
 ```
 `yc-sa-json-credentials` should contain JSON with authorized key for Service Account. More info in [Yandex Cloud IAM documentation](https://cloud.yandex.ru/docs/container-registry/operations/authentication#sa-json).
 
+Alternative usage:
+
+
+
 See [action.yml](action.yml) for the full documentation for this action's inputs and outputs.
 
 ## Prerequisites
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,33 @@
 import * as core from '@actions/core';
 import {fromServiceAccountJsonFile} from './service-account-json';
+import {IIAmCredentials} from '@yandex-cloud/nodejs-sdk/dist/types';
 import {IamTokenService} from '@yandex-cloud/nodejs-sdk/dist/token-service/iam-token-service';
+
+function getServiceAccountJson(): IIAmCredentials {
+  const ycSaJsonCredentials = core.getInput('yc-sa-json-credentials');
+  if (ycSaJsonCredentials.length !== 0) {
+    core.setSecret(ycSaJsonCredentials);
+    return fromServiceAccountJsonFile(JSON.parse(ycSaJsonCredentials));
+  }
+
+  const ycKeyId = core.getInput('yc-key-id', {required: true});
+  const ycServiceAccountId = core.getInput('yc-service-account-id', {required: true});
+  const ycPrivateKey = core.getInput('yc-private-key', {required: true});
+
+  core.setSecret(ycServiceAccountId);
+  core.setSecret(ycKeyId);
+  core.setSecret(ycPrivateKey);
+
+  return {
+    accessKeyId: ycKeyId,
+    serviceAccountId: ycServiceAccountId,
+    privateKey: ycPrivateKey,
+  };
+}
 
 async function run(): Promise<void> {
   try {
-    const ycSaJsonCredentials = core.getInput('yc-sa-json-credentials', {
-      required: true,
-    });
-    core.setSecret(ycSaJsonCredentials);
-
-    const serviceAccountJson = fromServiceAccountJsonFile(JSON.parse(ycSaJsonCredentials));
+    const serviceAccountJson = getServiceAccountJson();
 
     core.info('Function inputs set');
     const tokenService = new IamTokenService(serviceAccountJson);
